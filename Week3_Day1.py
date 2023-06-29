f182c5cbb07991ded6e17c9fe6a29fe70fe1ac84
SunShine2085 <136990672+SunShine2085@users.noreply.github.com>
2023-06-29 16:46:16 -0400

    Added activities from laptop
    Function flow activities

---
+++ Week3_Day1.py
@@ -0,0 +1,58 @@
+#Writing Functions 1
+#Chris Courville
+
+# def add(x,y):
+#     print("I will add the numbers %d and %d" % (x,y))
+#     return x + y;
+#
+# sum = add(5,7)
+# print("=", sum)
+#
+# def print_lyrics():
+#     print("Im a lumberjack, and I'm okay.")
+#     print("I sleep all night and I work all day.")
+# def repeat_lyrics():
+#     print_lyrics()
+#     print_lyrics()
+# repeat_lyrics()
+
+# x = float(input("Enter a number "))
+# y = float(input("Enter a number "))
+
+# def add(x,y):
+#     return x + y
+# print(add(x,y))
+
+# x = float(input("Enter a number "))
+# y = float(input("Enter a number "))
+
+# def mul(x,y):
+#     return x * y
+# # print(mul(x,y))
+#
+# def math(x,y):
+#     print("multiply = " , mul(x,y))
+#     print("add = " , add(x,y))
+#
+# print(math(x,y))
+
+# Reading Flowing Functions
+
+# def f(x):
+#     x = x-1
+#     return g(x)+1
+#
+# def g(x):
+#     return x*2
+#
+# def h(x):
+#     if x%2 == 1:    # x odd
+#         return f(x) + x
+#     else:           # x even
+#         return f(f(x))
+#
+# print(h(3))
+# print(h(4))
+# print(h(5))
+
+
