f8e50bb8f3bc9dd5f4c7658235f0e948b7b9e3b7
SunShine2085 <136990672+SunShine2085@users.noreply.github.com>
2023-07-03 12:38:50 -0400

    Update Day2.py
    Writing functions

--- Day2.py
+++ Day2.py
@@ -1,1 +1,12 @@
+# Writing functions
+# Chris Courville
+# 7/3/2023
 
+
+def header(text, surround):
+    length = len(text) + 4
+    print(surround * length)
+    print(surround,text,surround)
+    print(surround * length)
+
+header("Chris is going to be super succesful in life", "$")
