86caf86ccfbd5aa4ee5362c0d04e2b0bc47422f2
SunShine2085 <136990672+SunShine2085@users.noreply.github.com>
2023-07-03 13:21:59 -0400

    Update Day2.py
    Writing functions excercise

--- Day2.py
+++ Day2.py
@@ -3,10 +3,39 @@
 # 7/3/2023
 
 
-def header(text, surround):
-    length = len(text) + 4
-    print(surround * length)
-    print(surround,text,surround)
-    print(surround * length)
+# def header(text, surround):
+#     length = len(text) + 4
+#     print(surround * length)
+#     print(surround,text,surround)
+#     print(surround * length)
 
-header("Chris is going to be super succesful in life", "$")
+# header("Chris is going to be super succesful in life", "$")
+# header("The world is my oyster","^")
+# header("I love living","@")
+
+# Function problems
+
+# def absolute_difference(x,y):
+#     print(abs(x-y))
+#
+# absolute_difference(5,10)
+# absolute_difference(10,5)
+# absolute_difference(200,-200)
+
+
+def pyramid(char, n):
+    for i in range(1, n + 1):
+        print(char * i)
+
+
+def main():
+    print('pyramid("*", 2)')
+    pyramid("*", 2)
+
+    print('pyramid("+", 5)')
+    pyramid("+", 5)
+
+    print('pyramid("x", 10)')
+    pyramid("x", 10)
+
+main()
